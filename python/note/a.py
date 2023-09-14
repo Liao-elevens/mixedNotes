04a01f73da11a2287574a11a355c7c87f45f68cc
Liao-el <1499815843@qq.com>
2023-09-14 09:33:44 +0800

    0基础get

--- python/note/a.py
+++ python/note/a.py
@@ -11,15 +11,53 @@
 # result = eval(f"{num1}{op}{num2}")
 # print(f"计算结果为{result}")
 
-import os
-import pprint
-print(os.listdir())
-# 要保证文件路径地址
-with open("./24-demo.txt") as f:
-    file_data = f.readlines()
-    # file_data = f.read()
-    pprint.pprint(file_data)
-    print('统计全部行数: ', len(file_data))
-    print('统计空行行数: ', file_data.count("\n"))
-    print('统计单词 I 出现的次数: ', str(file_data).split(" ").count(
-        "I"), '== 错误用法，结果为0: ', file_data.count("I"))
+# import os
+# import pprint
+# print(os.listdir())
+# # 要保证文件路径地址
+# with open("./24-demo.txt") as f:
+#     file_data = f.readlines()
+#     # file_data = f.read()
+#     pprint.pprint(file_data)
+#     print('统计全部行数: ', len(file_data))
+#     print('统计空行行数: ', file_data.count("\n"))
+#     print('统计单词 I 出现的次数: ', str(file_data).split(" ").count(
+#         "I"), '== 错误用法，结果为0: ', file_data.count("I"))
+
+
+# list=[ 'a', 1, 'b', 2, 'c', 'c', 3 ]
+# # list[4:6]=[]
+# list.remove('c')
+# print('list_a: ',list)
+
+
+# list1 = [ 1, 2, 3]
+# tuple1 = ( 'abc', list1 )
+# print( type( tuple1 ))
+# dict1 = { tuple1, 1 } # 此语句是否能够正确执⾏？
+
+# data=input('输入：')
+# match data:
+#     case '1':
+#         print('1==')
+#     case '2':
+#         print('2==')
+#     case 'a':
+#         print('a==')
+#     case _:
+#         print('_==')
+
+# n=5
+# for i in range(n):
+#   print(i)
+
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(0, 4 * np.pi, 0.1)
+y = np.sin(x)
+plt.plot(x, y) #横轴使用x，纵轴使用y绘图
+plt.xlabel('x') #设定x轴的标签
+plt.ylabel('y') #设定y轴的标签
+plt.grid() #显示网格
+plt.show()
